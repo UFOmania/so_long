e9befed3c02a6e53993850f018fcaef8840cc589
UFOmania <130464178+UFOmania@users.noreply.github.com>
2025-03-06 23:06:14 +0000

    sl

--- src/map_utils.c
+++ src/map_utils.c
@@ -6,7 +6,7 @@
 /*   By: massrayb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 09:30:39 by massrayb          #+#    #+#             */
-/*   Updated: 2025/02/15 11:53:01 by massrayb         ###   ########.fr       */
+/*   Updated: 2025/03/06 23:00:13 by massrayb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ void	map_calculate_height(t_game_manager *gm, char *name)
 
 	fd = open(name, O_RDONLY);
 	if (fd == -1)
-		(ft_putendl_fd("Error: couldn't open the map\n", 2), exit(-1));
+		(ft_putendl_fd("Error: ", 2), ft_putendl_fd(strerror(errno), 2), \
+		exit(-1));
 	line = get_next_line(fd);
 	if (line && ft_strlen(line) > MAX_WIDTH)
 	{
